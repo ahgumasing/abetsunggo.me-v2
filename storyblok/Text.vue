fe898bbb14161f4ead29ff3b050abbfe15a13cde
Abette Gumasing <ahgumasing@gmail.com>
2023-04-25 17:30:15 +0800

    add text block

---
+++ storyblok/Text.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    blok: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <p class="text-4xl font-semibold">{{ blok.text }}</p>
+</template>
