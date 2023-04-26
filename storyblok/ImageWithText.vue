75a15dc5888df3862e79272964d97aef179eb5e2
Abette Gumasing <ahgumasing@gmail.com>
2023-04-26 15:07:50 +0800

    add image with text

---
+++ storyblok/ImageWithText.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    blok: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <a
+        class="flex items-center space-x-4 hover:text-gray-300"
+        :href="blok.link.url"
+    >
+        <img :src="blok.image" />
+        <p class="font-medium text-sm">{{ blok.text }}</p>
+    </a>
+</template>
